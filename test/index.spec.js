7d3372b1d9be6c7d446d141083c6655d13655338
Simranjeet Singh <simranjeet2589@gmail.com>
2020-02-22 13:41:38 +0530

    [Adding test for index]

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,23 @@
 const {expect} = require("chai");
 
-describe("The middleware", () => {
+const getLoggerMw = require("../src");
 
+const config = {
+  "name": "foo"
+};
+
+describe("The middleware", () => {
+  describe("when config is passed", () => {
+    it("should return a async function", () => {
+      const mw = getLoggerMw(config);
+      expect(typeof mw).to.equal("function");
+    });
+  });
+
+  describe("when config is not passed", () => {
+    it("should return a async function", () => {
+      const mw = getLoggerMw();
+      expect(typeof mw).to.equal("function");
+    });
+  });
 });
