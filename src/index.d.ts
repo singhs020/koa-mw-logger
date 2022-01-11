a30cf3b49c278b8936372a16c8a70e65cb0db598
Simranjeet Singh <12469462+singhs020@users.noreply.github.com>
2022-01-11 09:58:28 +0530

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,10 @@
+import { DefaultState, Middleware } from "koa";
+import { Logger as PinoLogger } from "pino";
+
 interface IConfig {
   name?: string;
   recordIp?: boolean;
   recordHeaders?: boolean;
 }
 
-export default function getLogger(config?: IConfig) : any;
+export default function getLogger(config?: IConfig) : Middleware<DefaultState, { ctx: { logger: Logger } }>;
