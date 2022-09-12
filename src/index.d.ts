e09ea5483d400072b7b59bfce60e7abc3765b8ac
Simranjeet Singh <simranjeet@s25.digital>
2022-09-12 16:03:05 +0530

    release patch - [fixing types]

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,12 @@
 import { DefaultState, Middleware } from "koa";
-import { Logger } from "pino";
+import { Logger as NativeLogger } from "pino";
 
 interface IConfig {
   name?: string;
   recordIp?: boolean;
   recordHeaders?: boolean;
 }
+
+export type Logger = NativeLogger;
 
 export default function getLogger(config?: IConfig) : Middleware<DefaultState, { ctx: { logger: Logger } }>;
